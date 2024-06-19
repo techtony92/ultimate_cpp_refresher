49317ac89beb087a5c41d50be5f9c1b9d581a23b
Tony <techtony92@gmail.com>
2024-06-18 23:18:19 -0400

    added lesson 3:consts

---
+++ 3_consts/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    /*
+     * Variables declared with const cannot be changed and must be defined at declaration.
+     */
+    const double pi  = 3.14;
+    std::cout << pi;
+
+    return 0;
+}
